6980ff47d55ac6eef6b154b6ae70e9e0fed7b26a
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-03-23 08:39:20 -0300

    Aula1 Filte

---
+++ funcoes/promise_1.js
@@ -0,0 +1,20 @@
+
+function primeiroElemento(array) {
+  return array[0];
+}
+
+function primeiraLetra(string) {
+  return string[0];
+}
+
+const letraMinuscula = letra => letra.toLowerCase();
+
+let p = new Promise(function (cumprirPromessa) {
+  cumprirPromessa(['Ana', 'Bia', 'Carlos', 'Daniel']);
+})
+
+p
+  .then(primeiroElemento)
+  .then(primeiraLetra)
+  .then(letraMinuscula)
+  .then(console.log)
