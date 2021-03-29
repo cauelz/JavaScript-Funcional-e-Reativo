70655b161f6f5e522c17def4694f6f8346778dcf
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-03-29 08:27:59 -0300

    Aula2 Promise

---
+++ funcoes/promise_2.js
@@ -0,0 +1,28 @@
+
+// setTimeout(function() {
+//     console.log('Executando callback...');
+//     setTimeout(function() {
+//         console.log('Executando callback...');
+//         setTimeout(function() {
+//             console.log('Executando callback...');
+//         })
+//     })
+// }, 2000);
+
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function (resolve) {
+        setTimeout(function() {
+            console.log('Executando promisse...');
+            resolve('vishhhhhh');
+        }, tempo)
+    })
+}
+
+esperarPor()
+    .then(() => esperarPor())
+    .then(esperarPor)
+// esperarPor(3000).then(texto => console.log(texto));
+
+
+
